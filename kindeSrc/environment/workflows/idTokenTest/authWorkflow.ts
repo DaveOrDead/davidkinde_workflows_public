6f7cd76de8088ed4442f451cc975b19bdcf5ea0a
Dave <dave@kinde.com>
2024-10-08 14:26:10 +1100

    Update authWorkflow.ts

--- kindeSrc/environment/workflows/idTokenTest/authWorkflow.ts
+++ kindeSrc/environment/workflows/idTokenTest/authWorkflow.ts
@@ -26,8 +26,8 @@ export default {
               }
         );
         
-        console.log('timeResponse', timeResponse.json.data);
-        kinde.idToken.setCustomClaim('sport', timeResponse.json.data[0].name);
+        console.log('timeResponse', timeResponse);
+        //kinde.idToken.setCustomClaim('sport', timeResponse.json.data[0].name);
         
         return 'testing add user token claim';
     },
