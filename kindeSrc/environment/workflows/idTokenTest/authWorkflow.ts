5f0bdc34633afcfce6f1d34628828a5c83d5ce8e
Dave <dave@kinde.com>
2024-10-08 14:13:40 +1100

    test fetch

--- kindeSrc/environment/workflows/idTokenTest/authWorkflow.ts
+++ kindeSrc/environment/workflows/idTokenTest/authWorkflow.ts
@@ -18,6 +18,20 @@ export const workflowSettings = {
 export default {
     async handle(event: any) {
         kinde.idToken.setCustomClaim('random', 'test');
+         const timeResponse = await kinde.fetch(
+        'https://worldtimeapi.org/api/timezone/Etc/UTC',
+        {
+            method: 'GET',
+            responseFormat: 'json',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }
+    );
+    console.log('timeResponse', timeResponse.json);
+        kinde.idToken.setCustomClaim('time', timeResponse.json);
+        
         return 'testing add user token claim';
     },
 
